3b057c2d0d90a3433ffd2aaf5b0573e5725693d6
TeHao <TeHao@TH_LENOVO>
2023-06-05 15:11:05 +0800

    Update

--- EmpowerTH_1/EmpowerTH_1.cs
+++ EmpowerTH_1/EmpowerTH_1.cs
@@ -68,7 +68,7 @@ namespace EmpowerTH_1
 		/// <param name="engine">Link with SLAutomation process.</param>
 		public void Run(IEngine engine)
 		{
-			engine.GenerateInformation("Hello World");
+			engine.GenerateInformation("Hello World, Skyline!!!!");
 		}
 	}
 }
